0eb1833549e17bd0fc2b330c257ebadc55f9a802
ulmentflam <johnwowen3@gmail.com>
2026-06-05 23:21:22 -0400

    Adding data

---
+++ data/tinyshakespeare.py
@@ -0,0 +1,100 @@
+# bin/python data/tinyshakespeare.py
+"""
+Downloads and tokenizes the Tiny Shakespeare dataset.
+- The download is from Karpathy's github repository.
+- The toknization is GPT-2 tokenizer via tiktoken.
+
+The output is written to a newly created tinyshakespear/ folder.
+
+
+"""
+
+import os
+import argparse
+
+import tiktoken
+from transformers import AutoTokenizer
+
+from utils import download_file, write_datafile
+
+DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), ".tinyshakespeare")
+
+TINY_SHAKESPEARE_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+FILE_NAME = "tiny_shakespeare.txt"
+VAL_FILE_NAME = "tiny_shakespeare_val.txt"
+TRAIN_FILE_NAME = "tiny_shakespeare_train.txt"
+VAL_SIZE = 32768
+
+
+def download() -> None:
+    """
+    Downloads the Tiny Shakespeare dataset from Karpathy's github repository.
+    """
+    os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+    input_txt_path = os.path.join(DATA_CACHE_DIR, FILE_NAME)
+    if not os.path.exists(input_txt_path):
+        download_file(TINY_SHAKESPEARE_URL, input_txt_path)
+        print(f"Downloaded Tiny Shakespeare dataset to {input_txt_path}")
+        return
+    print(f"Tiny Shakespeare dataset already downloaded to {input_txt_path}")
+
+
+def tokenize(model_desc: str = "gpt-2") -> None:
+    if model_desc == "gpt-2":
+        encoder = tiktoken.get_encoding("gpt2")
+
+        def encode(s: str) -> list[int]:
+            return encoder.encode(s)
+
+        eot = encoder._special_tokens["<|endoftext|>"]  # End of text token
+    elif model_desc == "llama-3":
+        tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B")
+        assert tokenizer is not None, "Failed to load Llama-3 tokenizer"
+
+        def encode(s: str) -> list[int]:
+            return tokenizer.encode(
+                s, add_special_tokens=False, verbose=False, split_special_tokens=True
+            )
+
+        eot = tokenizer.eos_token_id
+    else:
+        raise ValueError(f"Invalid model descriptor: {model_desc}")
+
+    filename = os.path.join(DATA_CACHE_DIR, FILE_NAME)
+    text = open(filename, "r").read()
+    sections = text.split("\n\n")
+    tokens = []
+    for i, s in enumerate(sections):
+        tokens.append(eot)
+        # there was a mild bug where I originally intended to remove \n\n, but instead just added
+        # the EOT right after each \n\n, so I'm keeping that behavior for backwards compatibility
+        # therefore we have to here add an extra \n\n at the end of each section, except the last
+        s_pad = s + "\n\n" if i != len(sections) - 1 else s
+        tokens.extend(encode(s_pad))
+    # Let's take the first 10% of the tokens for validation
+    val_tokens = tokens[:VAL_SIZE]
+    train_tokens = tokens[VAL_SIZE:]
+    write_datafile(
+        os.path.join(DATA_CACHE_DIR, TRAIN_FILE_NAME), train_tokens, model_desc
+    )
+    write_datafile(os.path.join(DATA_CACHE_DIR, VAL_FILE_NAME), val_tokens, model_desc)
+    print(
+        f"Tokenized Tiny Shakespeare dataset and wrote to {os.path.join(DATA_CACHE_DIR, TRAIN_FILE_NAME)} and {os.path.join(DATA_CACHE_DIR, VAL_FILE_NAME)}"
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Tiny Shakespeare dataset preprocessing"
+    )
+    parser.add_argument(
+        "-m",
+        "--model_desc",
+        type=str,
+        default="gpt-2",
+        choices=["gpt-2", "llama-3"],
+        help="Model type, gpt-2|llama-3",
+    )
+    args = parser.parse_args()
+    download()
+    tokenize(args.model_desc)
---
+++ data/utils.py
@@ -0,0 +1,138 @@
+"""
+Common utilities for data processing.
+"""
+
+from typing import Any
+
+import requests
+import numpy as np
+from tqdm import tqdm
+
+
+def download_file(url: str, output_path: str) -> None:
+    """Helper to download a file from a URL to a local path."""
+    response = requests.get(url, stream=True)
+    total = int(response.headers.get("content-length", 0))
+    with (
+        open(output_path, "wb") as file,
+        tqdm(
+            desc=output_path,
+            total=total,
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as bar,
+    ):
+        for data in response.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
+
+
+HEADERS_INFO: dict = {
+    "gpt-2": {
+        "magic": 20240520,
+        "version": 1,
+        "token_dtype": np.uint16,
+    },
+    "llama-3": {
+        "magic": 20240801,
+        "version": 7,
+        "token_dtype": np.uint32,
+    },
+}
+
+MAGIC_NUMBER = 20240522
+
+
+def write_datafile(filename: str, tokens: list[int], model_desc: str = "gpt-2") -> None:
+    """
+    Saves token data as a binary file, for reading in the target language.
+    - First comes a header with 256 int32s
+    - The tokens follow, each as uint16 or uint32, depending on the model.
+    """
+    assert len(tokens) < 2**31, "Too many tokens"  # ~2.1 billion tokens
+    assert model_desc in HEADERS_INFO, f"Invalid model description: {model_desc}"
+    header_info = HEADERS_INFO[model_desc]
+    magic = header_info["magic"]
+    version = header_info["version"]
+    token_dtype = header_info["token_dtype"]
+    header = np.zeros(256, dtype=np.int32)
+    header[0] = magic
+    header[1] = version
+    header[2] = len(tokens)
+    tokens_np = np.array(tokens, dtype=token_dtype)
+    num_bytes = (256 * 4) + (len(tokens) * tokens_np.itemsize)
+    print(
+        f"Writing {len(tokens):,} tokens to {filename} ({num_bytes:,} bytes) in the {model_desc} format"
+    )
+    with open(filename, "wb") as f:
+        f.write(header.tobytes())
+        f.write(tokens_np.tobytes())
+    print(f"Wrote {filename}")
+
+
+def write_evalfile(filename: str, datum: list[dict[str, Any]]) -> None:
+    """
+    Saves eval data as a .bin file, for reading in the target language.
+    Used for multiple-choice style evals, e.g. HellaSwag and MMLU
+    - First comes a header with 256 int32s
+    - The examples follow, each example is a stream of uint16_t:
+        - <START_EXAMPLE> delimiter of 2**16-1, i.e. 65,535
+        - <EXAMPLE_BYTES>, bytes encoding this example, allowing efficient skip to next
+        - <EXAMPLE_INDEX>, the index of the example in the dataset
+        - <LABEL>, the index of the correct completion
+        - <NUM_COMPLETIONS>, indicating the number of completions (usually 4)
+        - <NUM><CONTEXT_TOKENS>, where <NUM> is the number of tokens in the context
+        - <NUM><COMPLETION_TOKENS>, repeated NUM_COMPLETIONS times
+    """
+    header = np.zeros(256, dtype=np.int32)
+    header[0] = MAGIC_NUMBER  # magic number
+    header[1] = 1  # version
+    header[2] = len(datum)  # number of examples
+    header[3] = 0  # longest_example_bytes
+    longest_example_bytes = 0
+    full_stream = []  # The stream of uint16s, we write a single uint16 at a time at the end
+    assert len(datum) < 2**16, "Too many examples"
+    for idx, data in enumerate(datum):
+        stream = []
+        stream.append(2**16 - 1)  # <START_EXAMPLE> delimiter
+        stream.append(
+            0
+        )  # <EXAMPLE_BYTES>, bytes encoding this example, allowing efficient skip to next
+        stream.append(idx)  # <EXAMPLE_INDEX>, the index of the example in the dataset
+        stream.append(data["label"])  # <LABEL>, the index of the correct completion
+        ending_tokens = data["ending_tokens"]
+        assert len(ending_tokens) == 4, "Only 4 completions are supported"
+        stream.append(
+            len(ending_tokens)
+        )  # <NUM_COMPLETIONS>, indicating the number of completions (usually 4)
+        ctx_tokens = data["ctx_tokens"]
+        assert all(0 <= tok < 2**16 for tok in ctx_tokens), (
+            "Context tokens must be uint16"
+        )
+        stream.append(len(ctx_tokens))
+        stream.extend(ctx_tokens)
+        for end_tokens in ending_tokens:
+            assert all(0 <= tok < 2**16 for tok in end_tokens), (
+                "Ending tokens must be uint16"
+            )
+            stream.append(len(end_tokens))
+            stream.extend(end_tokens)
+        n_bytes = len(stream) * 2  # 2 bytes per uint16
+        assert n_bytes < 2**16, "Example too long"
+        stream[1] = (
+            n_bytes  # <EXAMPLE_BYTES>, bytes encoding this example, allowing efficient skip to next
+        )
+        longest_example_bytes = max(longest_example_bytes, n_bytes)
+        full_stream.extend(stream)
+    stream_np = np.array(full_stream, dtype=np.uint16)
+    assert 0 < longest_example_bytes < 2**16, "Longest example bytes must be uint16"
+    header[3] = longest_example_bytes
+    num_bytes = (256 * 4) + (len(full_stream) * 2)
+    print(
+        f"Writing {len(datum):,} examples to {filename} ({num_bytes:,} bytes) in the eval format"
+    )
+    with open(filename, "wb") as f:
+        f.write(header.tobytes())
+        f.write(stream_np.tobytes())
+    print(f"Wrote {filename}")
